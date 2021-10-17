7dbb2f5ac12064cf36bcfe5471fdb3e4cbdd3293
Davi Francisco da Silva <43782019+davif10@users.noreply.github.com>
2021-10-17 17:51:07 -0300

    -Recuperando localização do usuário

--- lib/Mapa.dart
+++ lib/Mapa.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Mapa extends StatefulWidget {
@@ -12,23 +13,65 @@ class Mapa extends StatefulWidget {
 class _MapaState extends State<Mapa> {
   Completer<GoogleMapController> _controller = Completer();
   Set<Marker>_marcadores ={};
+  CameraPosition _posicaoCamera = CameraPosition(
+      target: LatLng(-23.694588037846376, -46.7018715235196),
+      zoom: 18
+  );
 
   _onMapCreated(GoogleMapController controller){
     _controller.complete(controller);
   }
 
-  _exibirMarcador(LatLng latLng){
-    Marker marcador = Marker(
-        markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
-        position: latLng,
-      infoWindow: InfoWindow(
-        title: "Marcador"
-      )
+  _exibirMarcador(LatLng latLng) async{
+    List<Placemark> listaEnderecos = await Geolocator()
+        .placemarkFromCoordinates(latLng.latitude, latLng.longitude);
+    if(listaEnderecos != null && listaEnderecos.length > 0){
+      Placemark endereco = listaEnderecos[0];
+      String rua = endereco.thoroughfare;
+
+      Marker marcador = Marker(
+          markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
+          position: latLng,
+          infoWindow: InfoWindow(
+              title: rua
+          )
+      );
+      setState(() {
+        _marcadores.add(marcador);
+      });
+    }
+  }
+  
+  _movimentarCamera() async{
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+        CameraUpdate.newCameraPosition(_posicaoCamera)
     );
-    setState(() {
-      _marcadores.add(marcador);
+  }
+
+  _adicionarListenerLocalizacao(){
+    var geolocator = Geolocator();
+    var locationOptions = LocationOptions(accuracy: LocationAccuracy.high);
+    geolocator.getPositionStream(locationOptions).listen((position) {
+        setState(() {
+          _posicaoCamera = CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 18
+          );
+          _movimentarCamera();
+        });
     });
-    print("Local clicado: ${latLng}");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _adicionarListenerLocalizacao();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
@@ -41,10 +84,7 @@ class _MapaState extends State<Mapa> {
         child: GoogleMap(
           markers: _marcadores,
           mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(-23.694588037846376, -46.7018715235196),
-            zoom: 18
-          ),
+          initialCameraPosition: _posicaoCamera,
           onMapCreated: _onMapCreated,
           onLongPress: _exibirMarcador,
         ),
